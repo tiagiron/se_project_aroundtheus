cb86320f5022021e05c5e3ea29df1b67bd539a34
Tia Giron <tiagiron01@gmail.com>
2024-05-30 17:03:37 -0500

    modal box responsive

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileModalCloseBtn = document.querySelector("#modal-close-button");
+
+profileModalCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
